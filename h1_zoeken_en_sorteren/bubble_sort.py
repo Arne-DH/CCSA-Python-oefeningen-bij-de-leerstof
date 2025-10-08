080d19d151f95c79e24a1adff29c9b009752fcf9
Arne De Herdt <arne.deherdt@student.hogent.be>
2025-10-08 11:05:08 +0200

    buuble sort

--- h1_zoeken_en_sorteren/bubble_sort.py
+++ h1_zoeken_en_sorteren/bubble_sort.py
@@ -1,6 +1,15 @@
 def bubble_sort(a):
-    pass
+    n = len(a)
+    uitgevoerd = 0
+    for i in range (n-1):
+        for j in range (n-1, i, -1):
+            if(a[j-1] > a[j]):
+                a[j-1], a[j] = a[j],a[j-1]
+            uitgevoerd +=1
+        print(a)
+    print(f"Voor een rij van lengte {n} werd het if-statement {uitgevoerd} keer uitgevoerd")
 
 a = [int(_) for _ in input().split()]
 bubble_sort(a)
 
+print(a)
--- h1_zoeken_en_sorteren/iteratieve_versie_binair_zoeken.py
+++ h1_zoeken_en_sorteren/iteratieve_versie_binair_zoeken.py
@@ -1,3 +1,24 @@
 
-def zoek_binair():
-  pass
+def zoek_binair(rij,value):
+  l = 0
+  r = len(rij) -1
+
+  while l != r:
+    print(f"{l}, {r}")
+    m = int((l+r)/2)
+    if rij[m] < value:
+      l = m+1
+    else:
+      r = m
+  
+  if rij[l] == value:
+    i = l
+  else:
+    i = -1
+  return i
+
+
+#testen
+
+i = zoek_binair([0,10,20,30,40,50,60,70,80,90],70)
+print(f"index = {i}")
